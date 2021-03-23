c8f5023990feb168c1dc24fbeed7fad6fd89a706
Ashutosh-jos <80083167+Ashutosh-jos@users.noreply.github.com>
2021-03-23 23:11:19 +0530

    Create Optionpana.java

---
+++ Optionpana.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;  
+public class optionpane 
+{  
+JFrame f;  
+optionpane()
+{  
+    f=new JFrame();  
+    JOptionPane.showMessageDialog(f,"ERROR 404. FILE CANT BE OPEN");  
+}  
+public static void main(String[] args)
+ {  
+    new optionpane();  
+}  
+}
