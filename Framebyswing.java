355c27dbf4f3ac41019351a857d7165b354a2009
Ashutosh-jos <80083167+Ashutosh-jos@users.noreply.github.com>
2021-03-23 23:26:13 +0530

    Create Framebyswing.java

---
+++ Framebyswing.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+/**
+ * frameusingswing
+ */
+public class frameusingswing {
+
+    public static void main(String[] args) {
+        JFrame ob = new JFrame();
+        
+      ob.setSize(250,400);
+      ob.setVisible(true);
+      ob.setTitle("frame Using Swing");
+
+    }
+}
